c7356e8b5871d068f1bb986d9289f833e5c6761d
sarniraula <sar.niraula162@gmail.com>
2025-09-04 18:45:40 -0400

    Implement Local storage to store language change state.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,17 @@ import Header from "./components/Header";
 import Terms from "./components/Terms";
 
 function App() {
-  const [language, setLanguage] = useState("en");
+  //Get language option from local storage if its there
+  const [language, setLanguage] = useState(() => {
+    return localStorage.getItem("language") || "en";
+  });
   const [translations, setTranslations] = useState({});
 
   useEffect(() => {
+     // Save the current language to localStorage
+    localStorage.setItem("language", language);
+
+    // Fetch translations
     axios
       .get(`http://localhost:3000/translations/${language}`)
       .then((res) => setTranslations(res.data));
